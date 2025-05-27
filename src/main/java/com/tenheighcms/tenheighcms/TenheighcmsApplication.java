fdf033856a218fd852da93bc0beb5db0adfb94fa
dangialiang <dangialiang@gmail.com>
2025-05-27 19:32:02 +0800

    feat: first init

---
+++ src/main/java/com/tenheighcms/tenheighcms/TenheighcmsApplication.java
@@ -0,0 +1,13 @@
+package com.tenheighcms.tenheighcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenheighcmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenheighcmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenheighcms/tenheighcms/TenheighcmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenheighcms.tenheighcms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TenheighcmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
